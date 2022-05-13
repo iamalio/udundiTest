03ca5431271950b4da23ddffd11876f06a9fdd85
[Pedro Zurita] <[peterzurita27@gmail.com]>
2022-05-13 15:10:58 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<div id="app">
+<FrontPage />
+</div>
+</template>
+
+<script>
+import FrontPage from './components/FrontPage.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    FrontPage
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import'~bootstrap/dist/css/bootstrap.css'
+
+</style>
---
+++ src/components/FrontPage.vue
@@ -0,0 +1,31 @@
+<template>
+<section id="frontpage" class="container-fluid frontpage">
+  <div class="row">
+    <div class="frontpage__leftBar col-sm-3 col-md-3 col-lg-3">
+      <div class="frontpage__leftBarContent">
+        <h1 class="">Explore</h1>
+        <div class="frontpage__ctaWrapper">
+          <img src="../assets/Plus.svg" class="frontpage__ctaWrapperButton"/>
+          <p>More Details</p>
+        </div>
+      </div>
+    </div>
+  </div>
+<PopUp />
+</section>
+</template>
+
+<script>
+import PopUp from './PopUp.vue'
+
+export default {
+  name: 'FrontPage',
+   components: {
+      PopUp
+   }
+}
+</script>
+
+<style >
+@import '@/assets/_frontpage.scss';
+</style>
